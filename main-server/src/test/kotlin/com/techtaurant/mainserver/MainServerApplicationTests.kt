7c8ff82d41348fc4bb2abac4a444e4c45da8be96
rookedsysc <rookedsysc36@gmail.com>
2025-11-19 19:12:55 +0900

    spring boot init
    spring boot web, jpa, mysql, spring security

---
+++ main-server/src/test/kotlin/com/techtaurant/mainserver/MainServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.techtaurant.mainserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
