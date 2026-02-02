8814c3e9af613de9b6813fc846b5bfb2fdb90da7
rookedsysc <rookedsysc36@gmail.com>
2026-02-02 20:44:51 +0900

    feat: 스케줄러 잠금 및 스케줄링 활성화
    분산 환경에서 스케줄러 중복 실행 방지를 위한 설정 추가
    - @EnableScheduling: 스프링 스케줄링 활성화
    - @EnableSchedulerLock: ShedLock을 통한 분산 잠금 활성화
    - defaultLockAtMostFor: 최대 잠금 시간 10분 설정

--- main-server/src/main/kotlin/com/techtaurant/mainserver/MainServerApplication.kt
+++ main-server/src/main/kotlin/com/techtaurant/mainserver/MainServerApplication.kt
@@ -1,10 +1,14 @@
 package com.techtaurant.mainserver
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
 @SpringBootApplication
